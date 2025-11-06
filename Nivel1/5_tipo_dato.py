a9f7613d4b166cf65e05ced4f6aebc3ece70c5e2
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-06 12:17:52 -0500

    Ejercicio 5 - Nivel 1 - Tipos de datos

---
+++ Nivel1/5_tipo_dato.py
@@ -0,0 +1,17 @@
+cadena = "Juan Esteban Holguin"
+entero = 25
+numero_float = 2.5
+booleano = True
+lista = []
+tupla = ()
+diccionario = {}
+conjunto = {"Juan", 1}
+
+print(type(cadena).__name__)
+print(type(entero).__name__)
+print(type(numero_float).__name__)
+print(type(booleano).__name__)
+print(type(lista).__name__)
+print(type(tupla).__name__)
+print(type(diccionario).__name__)
+print(type(conjunto).__name__)
