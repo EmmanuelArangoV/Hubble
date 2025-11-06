6730fd7df6e5751dc922270e1bf932d1f93fa912
valtabpul <valtabpul@gmail.com>
2025-11-06 12:28:30 -0500

    Ejercicio 5, Nivel 1

---
+++ Nivel1/tipo_dato.py
@@ -0,0 +1,13 @@
+cadena = "valeria"
+entero = 20
+numero_float = 3.5
+tupla = ()
+lista = []
+diccionario = {}
+
+print(type(cadena).__name__)
+print(type(entero).__name__)
+print(type(numero_float).__name__)
+print(type(tupla).__name__)
+print(type(lista).__name__)
+print(type(diccionario).__name__)
