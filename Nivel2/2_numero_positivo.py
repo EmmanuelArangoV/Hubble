a47bb694775796102d0b246a1f81a8b9f1f5ee13
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-06 12:43:15 -0500

    Ejercicio 2 - Nivel 2 - Numeros positivos, negativos o cero

---
+++ Nivel2/2_numero_positivo.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingresar un número: "))
+
+if numero < 0:
+    print("El número es negativo")
+elif numero > 0:
+    print("El número es positivo")
+else:
+    print("El número es Cero")
