ec8294eaaad5dbfbb6141882c20bd35aa18c9f15
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-06 13:13:14 -0500

    Ejercicio 10 - Nivel 2 - Calculadora básica

---
+++ Nivel2/10_calculadora_basica.py
@@ -0,0 +1,18 @@
+#Solicitar número 1 y número 2 al usuario
+n1 = float(input("Ingresa un número: "))
+n2 = float(input("Ingresa otro número: "))
+
+simbolo = input("¿Qué operación quieres realizar? (+,-,*,/)")
+# Operaciones básicas
+if simbolo == "+":
+    #Suma
+    print("Suma: ", n1+n2)
+elif simbolo == "-":
+    #Resta
+    print("Resta: ", n1-n2)
+elif simbolo == "*":
+    #Multiplicación
+    print("Multuplicación: ", n1*n2)
+elif simbolo == "/":
+    #División
+    print("División: ", n1/n2)
