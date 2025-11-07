b1efacbe65c0d236e4d04103497eb04a1f31173d
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-07 14:00:12 -0500

    Ejercicio 26 - Nivel 5 - carrito de compras

---
+++ Nivel5/26_carrito_compras.py
@@ -0,0 +1,39 @@
+total_compra = 0
+carrito = []
+articulos = {
+    "camisa":{
+        "tipo": "camisa",
+        "talla": "xl",
+        "precio": 1200
+    },
+    "jean": {
+        "tipo": "jean",
+        "talla": 32,
+        "precio": 2000
+    },
+    "zapatos": {
+        "tipo": "zapatos",
+        "talla": 40,
+        "precio": 5000
+    }
+}
+while True:
+    print("1-Agregar \n-2 Eliminar \n-0 Salir")
+
+    r = int(input("Ingresa una opción ----> "))
+    match r:
+        case 1:
+            prod = input("Que producto desea agregar?\ncamisa\nJean\nzapatos\n ----> ").lower()
+            carrito.append(articulos[prod])
+        case 2: 
+            print("eliminar")
+        case 0:
+            break
+        case _:
+            print("opción no valida")
+print("----------------Tú Carrito de compras------------------")
+print("Articulo   ||   Precio")
+for art in carrito:
+    total_compra += art["precio"]
+    print(art["tipo"],               "||",   art["precio"])
+print("Total --->  ",total_compra) 
