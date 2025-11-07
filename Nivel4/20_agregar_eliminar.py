9760c618aaaf1effe9d587ddd7f59b7e71661ea3
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-07 08:23:46 -0500

    Ejercicio 20 - Nivel 4 Agregar y Eliminar frutas

--- Nivel4/20_agregar_eliminar.py
+++ Nivel4/20_agregar_eliminar.py
@@ -1,12 +1,20 @@
 frutas = [] #declarar una lista de frutas
 while True:
-    agregar = input("Ingresa una fruta: ")
-    if agregar != "0":
-        frutas.append(agregar)
-        print("Para salir escribe 0")
-    else:
-        break
+    add_delete = input("Agregar (A) / Eliminar (E): ")
+    print(add_delete.upper())
+    if add_delete == "A":
+        while True:
+            agregar = input("Ingresa una fruta: ")
+            if agregar != "0":
+                frutas.append(agregar)
+                print("Para salir escribe 0")
+            else:
+                break
+    elif add_delete == "B":
+        while True:
+            delete = input("Ingresar fruta: ")
         
+    
 print("-------FRUTAS-----------")
 for i in frutas: #Recorrer e imprimir la lista
     print(i)
