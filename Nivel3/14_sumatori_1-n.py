ed3e76b033c84b7e61b24dce329170871372d143
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-07 06:40:12 -0500

    Ejercicio 14 - Nivel 3 Sumatoria de 1-n

--- Nivel3/14_sumatori_1-n.py
+++ Nivel3/14_sumatori_1-n.py
@@ -2,4 +2,4 @@ n = int(input("Ingresar un número: ")) #Solicitar el número al usuario
 sumatoria = 0
 for i in range(1,n+1): #Usar el bucle se 1,n+1 para tomar el número que ingresó el usuario
     sumatoria += i #sumar el iterador a la variable sumatoria
-print(sumatoria)
+print(f"La sumatoria de los numeros del los números de 1 hasta {n} es: {sumatoria}")
