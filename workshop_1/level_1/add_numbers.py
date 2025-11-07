59b4e4935dbd854d2ce78de4f8c58ae4bc8b7583
EmmanuelArangoV <emmanuelarangov@gmail.com>
2025-11-06 20:02:54 -0500

    Implement addition calculator with input validation and repeat option

---
+++ workshop_1/level_1/add_numbers.py
@@ -0,0 +1,17 @@
+print("Welcome to Calculator (only for addition)")
+
+while True:
+    try :
+        number1 = float(input("Please enter a number:"))
+        number2 = float(input("Please enter a number:"))
+    except:
+        print("Please enter valid numbers")
+        continue
+
+    addition = number1 + number2
+    print(f"The result of adding {number1} and {number2} is: {addition}")
+
+    response = input("Would you like to continue. Press (Y) to continue: ").capitalize()
+    if response != "Y":
+        break
+
