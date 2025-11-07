0061adcb5a685f759c6b73ac2abe7a88291aef02
EmmanuelArangoV <emmanuelarangov@gmail.com>
2025-11-06 22:26:19 -0500

    Classify a grade between three categories; Failed, Approved, Excellent

---
+++ workshop_1/level_2/grades_classification.py
@@ -0,0 +1,22 @@
+print("Welcome to the Grades Classification")
+
+while True:
+    try:
+        grade = float(input("Enter your grade: "))
+
+        if 0 <= grade <= 2.9:
+            print(f"Your grade {grade} is classified as: Failed")
+        elif 2.9 < grade <= 4.0:
+            print(f"Your grade {grade} is classified as: Approved")
+        elif 4.0 < grade <= 5.0:
+            print(f"Your grade {grade} is classified as: Excellent")
+        elif grade >= 5.1 or grade < 0.0:
+            print(f"Your grade {grade} is classified as: Unavailable")
+
+    except ValueError:
+        print("Please enter a numeric value")
+        continue
+
+    response = input("Would you like to continue with another grade? Press (Y) to continue: ").capitalize()
+    if response != "Y":
+        break
