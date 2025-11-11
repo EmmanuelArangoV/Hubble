4daa5323359b66a0f13f4224271cb4c3ba7aedcf
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-11 07:54:41 -0500

    Hitoria de Usuario S2M1 Gestionar Inventario

--- Semana2/gestionar_inventario.py
+++ Semana2/gestionar_inventario.py
@@ -1,34 +1,31 @@
 product_list = []
 
-def CreateProduct():
+def validar_entradas(tipo, prompt, validacion_extra = None):
+    mesajes = {
+        int : "Ingresar un número y que sea mayor a 0",
+        float: "Ingresar un número y que sea mayor a 0",
+        str : "No ingresar un texto vacío"
+    }
+    while True:
+        try:
+            valor = tipo(input(prompt))
+            if validacion_extra and not validacion_extra(valor):
+                print(f"\033[31m{mesajes[tipo]}\033[0m")
+                continue    
+            return valor
+        except ValueError:
+            print(f"\033[31m{mesajes[tipo]}\033[0m")
+    
+def create_product():
     product = {}
-    while True: #validar el nombre del producto
-        try:
-            name_prod = input("Ingresar nombre del producto: ")
-            break
-        except ValueError:
-            print("Ingresa un valor válido para este campo")
+    
+    #solicitar 
+    name_prod = validar_entradas(str, "Ingresar nombre del producto: ", lambda x:len(x) > 0)
+    
+    price_product = validar_entradas(float, "Ingresar valor del producto: ", lambda x : x>0)
 
 
-    while True: #validar el precio del producto
-        try:
-            price_product = float(input("Ingresar valor del producto: "))
-            if price_product > 0:
-                break
-            else:
-                print("ingresar valor mayor a 0")
-        except ValueError:
-            print("Ingresa un valor válido para este campo")
-
-    while True: #validar la cantidad del producto
-        try:
-            amount_products = int(input("Ingresar cantidad de productos: "))
-            if amount_products > 0:
-                break
-            else:
-                print("ingresar valor mayor a 0")
-        except ValueError:
-            print("Ingresa un valor válido para este campo")
+    amount_products = validar_entradas(int, "Ingresar cantidad de productos: ", lambda x:x>0)
 
     product["name"] = name_prod
     product["price"] = price_product
@@ -36,14 +33,14 @@ def CreateProduct():
 
     return product
 
-def ShowInfo(products_list):
+def show_inventory(products_list):
     if len(products_list) == 0:
         print("\033[31mInventario vacío, Agrega productos para visualizarlos\033[0m")
     else:
         for product in products_list:
             print(f"Producto: {product["name"]} | Precio: {product["price"]}  | Cantidad: {product["amount"]}")
 
-def CalculateStats(products_list):
+def calculate_stats(products_list):
     total = 0
     sum_total = 0
     sum_products = 0
@@ -57,18 +54,19 @@ def CalculateStats(products_list):
         print(f"{product["name"]:<15}|{product["price"]:<10}|{product["amount"]:<10}|{total:<10}")
         print("--------------------------------------------------------------------------------------------")
         
-    print(f"{'Totales':<15}|{'':<10}|{sum_products:<10}|{total:<10}")
+    print(f"{'Totales':<15}|{'':<10}|{sum_products:<10}|{sum_total:<10}\n")
+    
 
 while True:
     try:
         res = int(input("¿Qué deseas hacer? \n1-Agregar producto \n2-Mostrar Inventario \n3-Calcular estadísticas \n0-Salir \n---> "))
         match res:
             case 1:
-                product_list.append(CreateProduct())
+                product_list.append(create_product())
             case 2:
-                ShowInfo(product_list)
+                show_inventory(product_list)
             case 3:
-                CalculateStats(product_list)
+                calculate_stats(product_list)
             case 0:
                 break
             case _:
