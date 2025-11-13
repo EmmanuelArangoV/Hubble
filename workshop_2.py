443d4cae4f17f7f813d103cac8a04000dca25c5b
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-13 10:56:51 -0500

    feature reports-patients

--- workshop_2.py
+++ workshop_2.py
@@ -15,32 +15,29 @@ patients = [
     },
 ]
 
-# def delete_patient(patients):
-#     band_patient_id = False
-#     while band_patient_id == False:
-#         band = False
-#         try:
-#             patient_id = int(input("Type a patient ID to delete: "))
-#         except ValueError:
-#             print("Type a valid ID")
-
-#         for i,patient in enumerate(patients):
-#             if  patient_id == patient.get("id") :
-#                 band = True
-#                 band_patient_id = True
-#                 confirm = input(f"You will eliminate the patient {patient["name"]}. Are you sure? Yes/No: ").lower()
-#                 if confirm == "yes":
-#                     patients.pop(1)
-#                     print(f"Patient {patient["name"]} was deleted")
-#                     break
-#                 else: 
-#                     print("Deleting canceled")
-#                     break
-
-#         print(f"{"" if band else "not found"}")     
-#         print(patients)
-
-# delete_patient(patients)
+def delete_patient(patients):
+    band_patient_id = False
+    while band_patient_id == False:
+        band = False
+        try:
+            patient_id = int(input("Type a patient ID to delete: "))
+        except ValueError:
+            print("Type a valid ID")
+        for i,patient in enumerate(patients):
+            if  patient_id == patient.get("id") :
+                band = True
+                band_patient_id = True
+                confirm = input(f"You will eliminate the patient {patient["name"]}. Are you sure? Yes/No: ").lower()
+                if confirm == "yes":
+                    patients.pop(1)
+                    print(f"Patient {patient["name"]} was deleted")
+                    break
+                else: 
+                    print("Deleting canceled")
+                    break
+        print(f"{"" if band else "not found"}")     
+        print(patients)
+delete_patient(patients)
 
 def reports(patients):
     file_name = "reports.txt"
