db75277057a70b1407460b8ce4612f7bdbfa7ab0
valtabpul <valtabpul@gmail.com>
2025-11-13 08:36:16 -0500

    register_patient

--- register_patient.py
+++ register_patient.py
@@ -2,23 +2,24 @@ patients = []
 def register_patients():
     while True:
 
-        patient_id = input("Enter the patient ID -> ")
+        print("... CLINIC MANAGER ...")
+        patient_id = input("\nEnter the patient ID -> ")
 
         flag = patient_exists(patient_id)
         if not flag:
             name = input("Enter your name -> ").capitalize()
             age = age_correct()
             gender = patient_gender()
-            diagnostic = input("Enter your diagnostic -> ")
+            diagnostic = diagnostics()
             record = record_events()
 
             patient = { "id": patient_id, "name": name, "age": age,
                 "gender": gender, "diagnostic": diagnostic, "record": record }
             patients.append(patient)
         else:
-            print("That  patient already exists")
+            print("\nThat  patient already exists")
 
-        continue_registration = input("Do you wish to register another patient? 'Y' to continue -> ").capitalize()
+        continue_registration = input("\nDo you wish to register another patient? 'Y' to continue -> ").capitalize()
 
         if continue_registration != "Y":
             break
@@ -40,11 +41,11 @@ def age_correct():
                 raise ValueError
             return age
         except ValueError:
-            print("invalid age")
+            print("\ninvalid age")
 
 
 def patient_gender():
-    gender = input("Enter your gender\n1) Male\n2) Female\n3) No binary\n-> ")
+    gender = input("\nEnter your gender\n1) Male\n2) Female\n3) No binary\n-> ")
 
     while True:
         if gender == "1":
@@ -54,7 +55,7 @@ def patient_gender():
         elif gender == "3":
             return "No binary"
         else:
-            print("Invalid gender")
+            print("\nInvalid gender")
             gender = input("Enter your gender\n1) Male\n2) Female\n3) No binary")
 
 
@@ -64,11 +65,30 @@ def record_events():
         event = input("Enter an event -> ")
         record.append(event)
 
-        choice = input("Do you want to add another event. Press 'Y' to continue-> ").capitalize()
+        choice = input("\nDo you want to add another event. Press 'Y' to continue-> ").capitalize()
 
         if choice != "Y":
             break
     return record
+
+def diagnostics():
+    menu = "\nEnter your diagnostic\n1) Hypertension\n2) Diabetes Mellitus\n3) Upper Respiratory Infection\n4) Urinary Tract Infection\n5) Allergic Rhinitis\n-> "
+    diagnostic = input(menu)
+
+    while True:
+        if diagnostic == "1":
+            return "Hypertension"
+        elif diagnostic == "2":
+            return "Diabetes Mellitus"
+        elif diagnostic == "3":
+            return "Upper Respiratory Infection"
+        elif diagnostic == "4":
+            return "Urinary Tract Infection"
+        elif diagnostic == "5":
+            return "Allergic Rhinitis"
+        else:
+            print("\nInvalid diagnostic")
+            diagnostic = input(menu)
 
 
 register_patients()
